c5c3f45f1bab60d15517d7de483b9943cf9760b8
xiabo <xiabo@pku.edu.cn>
2018-09-30 11:31:57 +0800

    init project except test files

---
+++ app/src/main/java/MyView.java
@@ -0,0 +1,23 @@
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class MyView extends View {
+    public MyView(Context context) {
+        super(context);
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        //TODO：
+    }
+
+
+}
---
+++ app/src/main/java/com/xinshen/reactnative/navigationview/NavigationViewActivity.java
@@ -0,0 +1,13 @@
+package com.xinshen.reactnative.navigationview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NavigationViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_view);
+    }
+}
